3af3c2b686a6884f468d59c8e70a17f8a3664e9e
alsigna <ignatov.alx@gmail.com>
2025-06-16 20:42:30 +0300

    add ruff

--- src/python2025_alexigna/main.py
+++ src/python2025_alexigna/main.py
@@ -1,2 +1,30 @@
+import io  # noqa: F401
+import os
+
+from scrapli import Scrapli
+
+
 def my_hello() -> None:
     print("hello")
+
+
+def concat(a: str, b: str) -> str:
+    """Конкатенация двух строк.
+
+    Args:
+        a (str): первая строка
+        b (str): вторая строка
+
+
+    Returns:
+        str: результат конкатенации
+    """
+    f = "test"
+    return a + b
+
+
+if __name__ == "__main__":
+    print(concat("11", "222"))
+    print(f"hello")
+    print(concat("11", "222"))
+    print(f"hello")
